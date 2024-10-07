51ca28adfa3a6db68b28eddf47b81ca078a992f5
Naisa <naisa@mail.ru>
2024-10-08 01:05:49 +0300

    бодавлена модель для бронирований и сделана миграция

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -12,6 +12,7 @@ from src.database import Base
 from src.models.hotels import HotelsOrm
 from src.models.rooms import RoomsOrm
 from src.models.users import UsersOrm
+from src.models.bookings import BookingsOrm
 
 config = context.config
 
