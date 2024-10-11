c7dff8721184c236ba00ec28b4314e1250872fbd
Naisa <naisa@mail.ru>
2024-10-12 00:52:22 +0300

    вернули пагинацию и фильтрацию

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -13,39 +13,55 @@ class HotelsRepository(BaseRepository):
     model = HotelsOrm
     schema = Hotel
 
-    async def get_all(
-        self,
-        location,
-        title,
-        limit,
-        offset,
-    ) -> list[Hotel]:
-        query = select( HotelsOrm )
-        if title:
-            query = query.filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
-        if location:
-            query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
-
-        query = (
-            query
-            .limit( limit )
-            .offset( offset )
-        )
-        print( query.compile(compile_kwargs={"literal_binds": True}))
-        result = await self.session.execute(query)
-        return [ Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
+    # async def get_all(
+    #     self,
+    #     location,
+    #     title,
+    #     limit,
+    #     offset,
+    # ) -> list[Hotel]:
+    #     query = select( HotelsOrm )
+    #     if title:
+    #         query = query.filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
+    #     if location:
+    #         query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
+    #
+    #     query = (
+    #         query
+    #         .limit( limit )
+    #         .offset( offset )
+    #     )
+    #     print( query.compile(compile_kwargs={"literal_binds": True}))
+    #     result = await self.session.execute(query)
+    #     return [ Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
 
 
     async  def get_filtered_by_time(
         self,
         date_from: date,
         date_to: date,
+        location: str,
+        title: str,
+        limit: int,
+        offset: int,
     ):
         rooms_ids_to_get = rooms_ids_for_booking( date_from=date_from, date_to=date_to )
+
         hotels_ids_to_get = (
-            select(RoomsOrm.hotel_id)
-            .select_from(RoomsOrm)
-            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+            select( RoomsOrm.hotel_id )
+            .select_from( RoomsOrm )
+            .filter( RoomsOrm.id.in_( rooms_ids_to_get ) )
+        )
+
+        if title:
+            hotels_ids_to_get = hotels_ids_to_get.filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
+        if location:
+            hotels_ids_to_get = hotels_ids_to_get.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
+
+        hotels_ids_to_get = (
+            hotels_ids_to_get
+            .limit( limit )
+            .offset( offset )
         )
 
         return await self.get_filtered(HotelsOrm.id.in_(hotels_ids_to_get))
