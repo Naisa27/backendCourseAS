ad03c492aac40d4c343c7216af88b86464ac6c63
Naisa <naisa@mail.ru>
2024-11-12 20:53:19 +0300

    check_test_mode отдельной фикстурой

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,12 @@ from src.models import *
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def async_main() -> None:
-    print("Я ФИКСТУРА")
+def check_test_mode() -> None:
     assert settings.MODE == "TEST"
 
+
+@pytest.fixture(scope="session", autouse=True)
+async def async_main(check_test_mode) -> None:
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
