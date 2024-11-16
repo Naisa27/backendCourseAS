0d7a9e30c30ab9478166fad941238b3c41db8f51
Naisa <naisa@mail.ru>
2024-11-16 23:44:19 +0300

    решение API тесты от Артема

--- tests/integration_tests/facilities/test_api.py
+++ tests/integration_tests/facilities/test_api.py
@@ -1,33 +1,23 @@
-# async def test_add_facilities(ac):
-#     response = await ac.post(
-#         "/facilities",
-#         headers={
-#             "Content-Type": "application/json"
-#         },
-#         json={
-#             "title": "wi-fi"
-#         }
-#     )
-#     print(f"{response.json()=}")
-#
-#     assert response.status_code == 200
-
-from src.schemas.facilities import FacilityAdd
-
-
-async def test_add_hotel(db):
-    facility_data = FacilityAdd(
-        title="интернет",
-    )
-    new_facility_data = await db.facilities.add(facility_data)
-    print(f"{new_facility_data=}")
-    await db.commit()
-
-
 async def test_get_facilities(ac):
-    response = await ac.get(
+    response = await ac.get("/facilities",)
+    print(f"{response.json()=}")
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+
+async def test_post_facilities(ac):
+    facility_title = "wi-fi"
+    response = await ac.post(
         "/facilities",
+        json={
+            "title": facility_title
+        }
     )
     print(f"{response.json()=}")
 
     assert response.status_code == 200
+    res = response.json()
+    assert isinstance( res, dict)
+    assert  res["data"]["title"] == facility_title
+    assert "data" in res
